1f0d4ee4b2ea72fe307c2109b817719956d5a080
Vnunezlm <vnunez.iccp@gmail.com>
2023-11-30 07:48:00 -0300

    ok

--- 01_HOLAMUNDO/bucles/Ejercicios_while/Ejercicio1/Ejercicio2/script.js
+++ 01_HOLAMUNDO/bucles/Ejercicios_while/Ejercicio1/Ejercicio2/script.js
@@ -1,3 +1,15 @@
+/*Suma del 1 al 10
+Desarrolla un programa que emplee
+un bucle while para calcular la suma
+de los números del 1 al 10.
+Muestra este resultado  página HTML */
+
 function sumar(){
-    
+    let contador = 0;
+    let suma = 0;
+while (contador < 10){
+    contador++;
+    suma += contador;
+}
+alert(`suma: ${suma}`);
 }
---
+++ 01_HOLAMUNDO/bucles/Ejercicios_while/Ejercicio1/Ejercicio3/script.js
@@ -0,0 +1,7 @@
+/*Primeros 10 números pares.
+Crea un programa utilizando un bucle while 
+para almacenar en un arreglo los primeros 
+10 pares */
+function numerosPares(){
+    let 
+}
